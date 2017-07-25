5bd733a52225431dca5cb02089f0b2cc4ba89164
Morgan Paine <mrgnpaine@gmail.com>
2017-07-25 04:42:53 +0000

    hi

---
+++ AmericanFlag.py
@@ -0,0 +1,106 @@
+import sys
+count = 1  
+while count <=10:
+    sys.stdout.write(" * ")
+    count = count +1
+import sys
+count = 1  
+while count <=30:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=10:
+    sys.stdout.write(" * ")
+    count = count +1
+import sys
+count = 1  
+while count <=30:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=10:
+    sys.stdout.write(" * ")
+    count = count +1
+import sys
+count = 1  
+while count <=30:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=10:
+    sys.stdout.write(" * ")
+    count = count +1
+import sys
+count = 1  
+while count <=30:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=10:
+    sys.stdout.write(" * ")
+    count = count +1
+import sys
+count = 1  
+while count <=30:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+import sys
+count = 1  
+while count <=60:
+    sys.stdout.write("=")
+    count = count +1
+sys.stdout.write('\n')
+
+
+    
